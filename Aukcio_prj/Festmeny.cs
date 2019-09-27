73df76aff6bb2d378d663dd32b6c37f0dd9f26c9
KarsaBoldizsár <karsa.boldi@gmail.com>
2019-09-27 13:10:17 +0200

    elso nehany feladat

---
+++ Aukcio_prj/Festmeny.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aukcio_prj
+{
+    class Festmeny
+    {
+        string cim;
+        string festo;
+        string stilus;
+
+
+
+    }
+}
---
+++ Aukcio_prj/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aukcio_prj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
